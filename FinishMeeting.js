fd61f94306e3c121168deb762808d22ac49d9dd8
Gnopps <ibjiba@fastmail.com>
2022-03-28 10:47:07 +0300

    Create FinishMeeting.js

---
+++ FinishMeeting.js
@@ -0,0 +1,34 @@
+module.exports = async function moveFilesWithTag(params) {
+  const { app } = params;
+
+  // Get file
+  const activeFile = this.app.workspace.getActiveFile();
+
+  // Update meeting status
+
+  let tags = await app.plugins.plugins["metaedit"].api.getPropertyValue(
+    "tags",
+    activeFile
+  );
+
+  let udpatedTags = tags.replace("future", "held");
+
+  let outcome = await app.plugins.plugins["metaedit"].api.update(
+    "tags",
+    udpatedTags,
+    activeFile
+  );
+
+  // Get customer name and move to its folder
+  let customerLink = await app.plugins.plugins["metaedit"].api.getPropertyValue(
+    "Project",
+    activeFile
+  );
+
+  let pureCustomerName = customerLink.match(/\[\[(.*?)\]\]/)[1];
+
+  await app.fileManager.renameFile(
+    activeFile,
+    "🏙️ Customers/" + pureCustomerName + "/" + activeFile.name
+  );
+};
